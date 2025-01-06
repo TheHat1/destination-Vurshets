5974032e05b3b97e754dda645b4fc9d78c7f922a
TheHat1 <gfs.74.bg@gmail.com>
2025-01-06 16:57:27 +0200

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
+import { Route, Routes } from "react-router-dom"
+import Logo from "./assets/destination-vurshets-logo.png"
+import Home from "./pages/Home.jsx"
 
 function App() {
 
   return (
     <>
-      <div className="bg-slate-900 w-screen h-24">
-
+      <div className="bg-slate-900 w-screen h-[110px] flex items-center justify-between fixed top-0">
+        <img className="pl-[10px]" src={Logo}/>
       </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+
+    return(
+        <>
+        <div className="bg-gray-300 h-screen w-screen"></div>
+        </>
+    )
+}
