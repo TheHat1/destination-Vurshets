e8cdd9b963b261934b1169b5d8dae4e49e508a55
TheHat1 <gfs.74.bg@gmail.com>
2025-01-08 18:34:02 +0200

    Add user scroll detection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
-export default function HomePage(){
 
+import { useEffect, useState } from "react"
+
+
+export default function HomePage(){
+    const [scrollValue, setScrollValue] = useState(screen.width)
+
+    function onUserScroll(scrollObject){
+       if(scrollObject.deltaY < 0){
+        
+        setScrollValue(scrollValue + 10)
+
+       }else{
+
+        setScrollValue(scrollValue - 10)
+
+       }
+    }
+    
     return(
         <>
-        <div className="bg-gray-300 h-screen w-screen"></div>
+        <div className="bg-gray-400 w-screen h-screen" onWheel={onUserScroll} >
+            
+        </div>
         </>
     )
 }
