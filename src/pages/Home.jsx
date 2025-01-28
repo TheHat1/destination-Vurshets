bbbef921bc71494a1495fcc8c56d73123fc90da9
TheHat1 <gfs.74.bg@gmail.com>
2025-01-28 13:23:19 +0200

    fix map zoom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,31 @@ import { useEffect, useState } from "react"
 
 export default function HomePage(){
     const [scrollValue, setScrollValue] = useState(screen.height)
-    let tempVar = 0
 
     function onUserScroll(scrollObject){
-       if(scrollObject.deltaY < 0){
-        
-        setScrollValue(scrollValue + 40)
-
-       }else{
-
-        setScrollValue(scrollValue - 40)
-
-       }
+      if(screen.height * 3 > scrollValue){ 
+        if(scrollObject.deltaY < 0){
+            setScrollValue(scrollValue + 40)
+           }
+      }
+      if(screen.height / 3 < scrollValue){
+        if(scrollObject.deltaY > 0){
+            setScrollValue(scrollValue - 40)
+        }
+      }
     }
 
     useEffect(()=>{
-        document.getElementById("Map").style.width = scrollValue
-        document.getElementById("Map").style.height = scrollValue
+        if(scrollValue < screen.height * 3 && scrollValue > screen.height /2){
+            document.getElementById("Map").style.width = scrollValue
+            document.getElementById("Map").style.height = scrollValue        
+        }
+
     }, [scrollValue])
-    
+
     return(
         <>
-        <div className="bg-gray-300 w-screen h-screen overflow-hidden flex justify-center items-center relative" onWheel={onUserScroll} >
+        <div className="bg-gray-300 w-[300vw] h-[300vh] fixed top-[60vh] left-[60vw] -translate-x-1/2 -translate-y-1/2 overflow-hidden flex justify-center items-center" onWheel={onUserScroll}>
            <Map id="Map" />
         </div>
         </>
