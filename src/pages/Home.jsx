4f338b8463d15f33419935bae54790a5fe553e11
TheHat1 <gfs.74.bg@gmail.com>
2025-01-29 09:50:44 +0200

    Add mouse tracking

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react"
 
 export default function HomePage(){
     const [scrollValue, setScrollValue] = useState(screen.height)
+    const [isHoldingMouse1, setIsHoldingMouse1] = useState(false)
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
 
     function onUserScroll(scrollObject){
       if(screen.height * 3 > scrollValue){ 
@@ -11,7 +13,7 @@ export default function HomePage(){
             setScrollValue(scrollValue + 40)
            }
       }
-      if(screen.height / 3 < scrollValue){
+      if(screen.height / 2 < scrollValue){
         if(scrollObject.deltaY > 0){
             setScrollValue(scrollValue - 40)
         }
@@ -23,12 +25,29 @@ export default function HomePage(){
             document.getElementById("Map").style.width = scrollValue
             document.getElementById("Map").style.height = scrollValue        
         }
-
     }, [scrollValue])
+
+    useEffect(()=>{
+       if(isHoldingMouse1){
+         const handleMouseMove = (mouseXY) => {
+             setMousePosition({ x: mouseXY.clientX, y: mouseXY.clientY });
+           }
+      
+           window.addEventListener("mousemove", handleMouseMove)
+      
+           return () => {
+             window.removeEventListener("mousemove", handleMouseMove)
+           }
+       }
+    })
 
     return(
         <>
-        <div className="bg-gray-300 w-[300vw] h-[300vh] fixed top-[60vh] left-[60vw] -translate-x-1/2 -translate-y-1/2 overflow-hidden flex justify-center items-center" onWheel={onUserScroll}>
+        <div className="bg-white w-[400px] h-[calc(100vh-var(--navbar-height))] fixed bottom-0" style={{ "--navbar-height": "110px" }}>
+            {isHoldingMouse1 ? <h1>holding mouse 1</h1>:<h1>Not holding mouse 1</h1>}
+            <h1>X:{mousePosition.x} Y:{mousePosition.y} </h1>
+        </div>
+        <div className="bg-gray-300 w-[300vw] h-[300vh] fixed top-[60vh] left-[60vw] -translate-x-1/2 -translate-y-1/2 overflow-hidden flex justify-center items-center -z-10" onWheel={onUserScroll} onMouseDown={()=>{setIsHoldingMouse1(true)}} onMouseUp={()=>{setIsHoldingMouse1(false)}}>
            <Map id="Map" />
         </div>
         </>
