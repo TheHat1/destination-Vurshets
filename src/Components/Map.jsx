06f11b28db2d2f347d1ddaf6dbf41a3f3914b005
TheHat1 <gfs.74.bg@gmail.com>
2025-02-19 19:37:22 +0200

    Add markers and new map

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,9 +1,15 @@
 import { MapContainer, Marker, ImageOverlay } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
 import { useEffect, useState } from "react"
-import { LatLngBounds, CRS } from "leaflet"
+import { LatLngBounds, CRS, Icon } from "leaflet"
+import { useNavigate } from "react-router-dom"
 
 export default function Map(){
+    const navigate = useNavigate()
+    const myMarker = new Icon({
+        iconUrl:"https://cdn-icons-png.flaticon.com/128/684/684908.png",
+        iconSize: [35,35]
+    })
     const [isMounted, setIsMounted] = useState(false)
     const bounds = new LatLngBounds(
         [0,0],
@@ -25,7 +31,10 @@ export default function Map(){
             maxBounds={[[0, 0], [4500, 4500]]} 
             className="h-screen w-[calc(100vw-var(--side-panel-width))] fixed z-0 right-0" style={{ "--side-panel-width": "500px" }}
         >
-                <ImageOverlay url="/src/assets/map.svg" bounds={bounds}/>
+            <ImageOverlay url="/src/assets/map.svg" bounds={bounds}/>
+            <Marker eventHandlers={{click:()=>{navigate("/paleopark")}}} position={[2070,2456]} icon={myMarker}/>
+            <Marker eventHandlers={{click:()=>{navigate("/vujenpark")}}} position={[1915,2420]} icon={myMarker}/>
+            <Marker eventHandlers={{click:()=>{navigate("/banq")}}} position={[2135,2400]} icon={myMarker}/>
         </MapContainer>
     );
 }
