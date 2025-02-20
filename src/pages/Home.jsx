e4f25c1833403826c95a98a73ff270c90f20f23f
TheHat1 <152319491+TheHat1@users.noreply.github.com>
2025-02-20 07:33:23 +0000

    Add searchbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,15 @@ import { useParams } from "react-router-dom"
 
 
 export default function HomePage(){
+  const locations = cardsInfo.locations
   const [locationCards, setLocationCards] = useState([
-    cardsInfo.locations.map((locations)=>{
+    locations.map((locations)=>{
       return(
         <LocationCard id={locations.id} imagePath={locations.imagePath} locationName={locations.locationName} locationDesc={locations.locationDesc}/>
       )
     })
   ])
-  const [inputValue, setInputValue] = useState()
+  const [inputValue, setInputValue] = useState("")
   const [isLoadedLocation, setIsLoadedLocation] = useState(false)
   const {id} = useParams()
 
@@ -27,12 +28,32 @@ export default function HomePage(){
       }
     },[id])
 
+    useEffect(()=>{
+      let searchInput = inputValue.toString().toLowerCase().replaceAll(' ','')
+      let searchResult
+      if(inputValue != ""){
+        searchResult = locations.filter((e)=>{return e.locationName.toString().toLowerCase().replaceAll(' ','').includes(searchInput)})
+        setLocationCards(searchResult.map(
+          (locations)=>{
+            return(
+              <LocationCard id={locations.id} imagePath={locations.imagePath} locationName={locations.locationName} locationDesc={locations.locationDesc}/>
+            )
+          })
+        )
+      }else{
+        searchResult = locations
+        setInputValue(" ")
+      }
+
+    }, [inputValue])
+
     return(
         <>
-        <div className="bg-white z-10 w-[500px] h-[calc(100vh-var(--navbar-height))] fixed bottom-0 shadow-[5px_0_10px_rgba(0,0,0,0.2)] overflow-y-auto overflow-x-hidden" style={{ "--navbar-height": "110px" }}>
-          <div className=" w-full h-[50px] border border-gray-900 ">
-            <input className="border-none w-full h-full" type="text" value={inputValue} onChange={(value1)=>{setInputValue(value1.target.value)}} placeholder="Търсене..."/>
-          </div>
+        <div className="mt-[110px] w-[500px] h-[50px] border border-gray-900 fixed z-50 ">
+           <input className="border-none w-full h-full" type="text"  onChange={value1=>{setInputValue(value1.target.value)}} placeholder="Търсене..."/>
+        </div>
+        <div className="bg-white z-10 w-[500px] h-[calc(100vh-var(--navbar-height))] fixed bottom-0 shadow-[5px_0_10px_rgba(0,0,0,0.2)] overflow-y-auto overflow-x-hidden" style={{ "--navbar-height": "160px" }}>
+
           {locationCards}
         </div>
         {isLoadedLocation ? <LocationViewer/> : <Map/>}
