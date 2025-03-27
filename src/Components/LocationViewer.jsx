1c61480de3e5222142054fa0191a2a04f77e3dfe
TheHat1 <152319491+TheHat1@users.noreply.github.com>
2025-03-27 06:54:50 +0000

    Add rounded edges to 3D viewer fr this time

--- src/Components/LocationViewer.jsx
+++ src/Components/LocationViewer.jsx
@@ -105,7 +105,7 @@ export default function LocationViewer(){
             </div>
             <h1 className="p-10 text-xl text-pretty indent-7 font-robotoMono">{t(locationDesc)}</h1>
             <Suspense fallback={PanoramicSkeleton}>
-                <div className="m-5 h-[600px] bg-gray-400 relative rounded-md">
+                <div className="m-5 h-[600px] bg-gray-400 relative rounded-md shadow-lg">
                     
                     <div onClick={()=>{setIsUserTried3D(true)}} className={`text-white rounded-md text-3xl bg-black opacity-75 pt-[40px] cursor-pointer absolute h-[600px] w-full ${
                     isUserTried3D ? "-z-50" : "z-50"}`}>
@@ -120,7 +120,7 @@ export default function LocationViewer(){
 
                     </div>
 
-                    <Canvas camera={{position: [0, 0, 0.1], fov: 45}}>
+                    <Canvas className="rounded-md" camera={{position: [0, 0, 0.1], fov: 45}}>
                         <OrbitControls reverseHorizontalOrbit reverseVerticalOrbit rotateSpeed={0.25} minPolarAngle={Math.PI / 2.095} maxPolarAngle={Math.PI / 1.915} enableZoom={false}/>
                         <PanoramicViewer texture={texturePath}/>
                     </Canvas>
